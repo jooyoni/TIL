06a2bef99da9c0513e3c2c69e5667d96e242f856
jooyeon Lee <ljyeon0713@naver.com>
2022-04-25 20:26:14 +0900

    redux를 이용한 todoList

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,22 +1,50 @@
-import {createStore} from "redux";
+import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add=document.getElementById("add");
-const minus=document.getElementById("minus");
-const number=document.querySelector("span");
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-
-const countModifier=(state=0, action)=>{
-  switch(action.type){
-    case "add":
-      return state+1;
-    case "minus":dfsdfds
-      return state-1;
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TODO:
+      return [ {text:action.text, id:Date.now()},...state];
+    case DELETE_TODO:
+      return [...state.filter((item)=>item.id!==action.id)];
     default:
       return state;
   }
-} 
-const countStore=createStore(countModifier);
-countStore.subscribe(()=>number.innerText=countStore.getState())
-number.innerText=countStore.getState();
-add.addEventListener("click", ()=>countStore.dispatch({type:"add"}))
-minus.addEventListener("click", ()=>countStore.dispatch({type:"minus"}))
+};
+
+const store = createStore(reducer);
+
+const deleteTodo=(id)=>{
+  store.dispatch({type:"DELETE_TODO", id:id})
+}
+
+store.subscribe(()=>{
+  const toDos=store.getState();
+  ul.innerHTML="";
+  toDos.forEach(todo=>{
+    const li=document.createElement("li");
+    const btn=document.createElement("button");
+    btn.addEventListener("click", ()=>{deleteTodo(todo.id)});
+    btn.innerText="DEL";
+    li.id=todo.id;
+    li.innerText=todo.text;
+    
+    li.appendChild(btn);
+    ul.appendChild(li)
+  })
+})
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, text: toDo });
+  console.log(store.getState())
+};
+
+form.addEventListener("submit", onSubmit);
