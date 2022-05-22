fe1794f860145fdd9724dfef29e287bd93f131a1
jooyeon Lee <ljyeon0713@naver.com>
2022-05-22 23:30:39 +0900

    특정 스크롤 위치에서 엘리먼트 애니메이션으로 등장시키기

--- interactive-web/src/App.js
+++ interactive-web/src/App.js
@@ -1,23 +1,41 @@
 import { motion, useAnimation, useViewportScroll } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import Scroll from "./components/Scroll";
 
 
 function App() {
-  const {scrollYProgress}=useViewportScroll();
+  const {scrollY, scrollYProgress}=useViewportScroll();
   const scrollAni=useAnimation();
+  const imgRef=useRef();
+  const [seeImg, setSeeImg]=useState(false);
+  const imgAni=useAnimation();
   useEffect(()=>{
     scrollYProgress.onChange(()=>{
       scrollAni.start({opacity:1 ,top:`calc( ${scrollYProgress.current*100}% - ${scrollYProgress.current*30}px`,transition:{type:"linear",duration:0.1}}).then(()=>scrollAni.start({opacity:0, transition:{delay:0.5}}));
     })
-  },[scrollYProgress])
+    scrollY.onChange(()=>{
+      if(scrollY.get()+window.innerHeight>imgRef.current.offsetTop+70)
+        setSeeImg(true)
+      if(scrollY.get()+window.innerHeight<imgRef.current.offsetTop+70)
+        setSeeImg(false);
+    })
+  },[scrollYProgress]);
+  useEffect(()=>{
+    if(seeImg)
+      imgAni.start({x:"500px"})
+    else
+      imgAni.start({x:"-100%", transition:{duration:0}})
+  },[seeImg])
+  useEffect(()=>{
+    console.log(imgRef.current.offsetTop)
+  },[imgRef]);
   return (
     <>
       <div style={{height:"200vh", backgroundColor:"red"}}>
         df
       </div>
-      <div style={{height:"200vh"}} />
+      <motion.img ref={imgRef} src="/logo192.png" initial={{x:"-100%"}} animate={imgAni} transition={{duration:1}} />
       <Scroll />
     </>
   );
